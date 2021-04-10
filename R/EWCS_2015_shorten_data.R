265c24eeb3f76ccb4fcf8bfb339a42ea957cb69d
Jason Moy <jasonmoy28@gmail.com>
2021-04-11 00:04:28 +0800

    type: <data> action: <added EWCS_2015_shorten package data>

---
+++ R/EWCS_2015_shorten_data.R
@@ -0,0 +1,22 @@
+#' Shortened Version of EWCS 
+#'
+#' Shortened Version of European Working Condition Survey (EWCS) suited for multilevel modeling. A dataset contains several work-relate variables 
+#'
+#' @format A data frame with 1500 rows and 13 variables:
+#' \describe{
+#'   \item{id}{Subject ID}
+#'   \item{Country}{Country}
+#'   \item{Gender_Individual}{Gender of the subject (individual-level)}
+#'   \item{Age_Individual}{Age of the subject (individual-level)}
+#'   \item{Education_Individual}{Education level of the subject (individual-level)}
+#'   \item{JS_Individual}{Job satisfaction scale (individual-level)}
+#'   \item{SWB_Individual}{Subjective well-being scale (individual-level)}
+#'   \item{Hofstede_IC_Country}{Hofstede measurement on Individualism-Collectivism (country-level)}
+#'   \item{Hofstede_UA_Country}{Hofstede measurement on Uncertainty Avoidance (country-level)}
+#'   \item{Hofstede_PD_Country}{Hofstede measurement on Power Distance (country-level)}
+#'   \item{Hofstede_MF_Country}{Hofstede measurement on Masculinity Feminity (country-level)}
+#'   \item{Hofstede_LO_Country}{Hofstede measurement on Long-term Orientation (country-level)}
+#'   \item{Hofstede_IR_Country}{Hofstede measurement on Indulgence Restraint (country-level)}
+#' }
+#' @source \url{http://www.diamondse.info/}
+"EWCS_2015_shorten"
