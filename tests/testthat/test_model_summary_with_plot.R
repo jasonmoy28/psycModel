65493d15672b37a3ab7f896b890c15cfb0768be0
Jason Moy <jasonmoy28@gmail.com>
2021-04-11 18:14:51 +0800

    type: <test> action: <added test for tidyselect syntax in model_summary_with_plot and lme_model>

--- tests/testthat/test_model_summary_with_plot.R
+++ tests/testthat/test_model_summary_with_plot.R
@@ -1,14 +1,39 @@
-model1 = model_summary_with_plot(response_variable = 'incidence',
-                                 level_1_factors = 'size',
-                                 level_2_factors = 'herd',
-                                 id = 'period',
-                                 data = lme4::cbpp,
-                                 use_package = 'lmerTest', #use lmerTest
-                                 model_performance = NULL,
-                                 return_result = T)
+########################################## Correct usage ##############################################################################
+model_summary_with_plot(response_variable = 'JS_Individual',
+                        level_1_factors = Age_Individual,
+                        level_2_factors = tidyselect::contains('Country'),
+                        id = 'Country',
+                        data = EWCS_2015_shorten,
+                        print_result = NULL,
+                        return_result = T)
 
-testthat::expect(is.null(model1$plot),failure_message = 'plot is not NULL when no interaction is included')
+model_summary_with_plot(response_variable = 'JS_Individual',
+                        level_1_factors = Age_Individual,
+                        level_2_factors = tidyselect::contains('Country'),
+                        id = 'Country',
+                        data = EWCS_2015_shorten,
+                        print_result = NULL,
+                        return_result = T)
 
+model_summary_with_plot(response_variable = 'JS_Individual',
+                        level_1_factors = Gender_Individual:Age_Individual,
+                        level_2_factors = Hofstede_IC_Country,
+                        id = 'Country',
+                        data = EWCS_2015_shorten,
+                        print_result = NULL,
+                        return_result = T)
+
+model_summary_with_plot(response_variable = 'JS_Individual',
+                        level_1_factors = c(Gender_Individual,Age_Individual),
+                        level_2_factors = tidyselect::contains('Country'),
+                        two_way_interaction_factor = contains('Individual'),
+                        id = 'Country',
+                        data = EWCS_2015_shorten,
+                        print_result = NULL,
+                        return_result = T)
+
+
+########################################## Incorrect usage ##############################################################################
 testthat::expect_warning(
   model_summary_with_plot(response_variable = 'JS_Individual',
                           level_1_factors = 'SWB_Individual',
@@ -18,4 +43,9 @@ testthat::expect_warning(
                           model_performance = NULL,
                           return_result = F),
   regexp = 'optimizer')
+
+testthat::expect_error(model_summary_with_plot(response_variable = 'JS_Individual',
+                        level_2_factors = tidyselect::contains('Country'),
+                        id = 'Country',
+                        data = EWCS_2015_shorten))
 
