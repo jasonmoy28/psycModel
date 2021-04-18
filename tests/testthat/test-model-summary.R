605874cf92ff6219ca50103dc94312a163beb217
Jason Moy <jasonmoy28@gmail.com>
2021-04-19 01:37:36 +0800

    type: <test> action: <updated test>

--- tests/testthat/test-model-summary.R
+++ tests/testthat/test-model-summary.R
@@ -14,3 +14,21 @@ expect_false(is.null(summary$model_summary))
 expect_false(is.null(summary$model_performance_df))
 expect_false(is.null(summary$assumption_plot))
 })
+
+testthat::test_that(desc = 'model summary test',{
+  model <- expect_warning(lme_model(
+    data = popular,
+    response_variable = popular,
+    random_effect_factors =  sex,
+    non_random_effect_factors = c(extrav,sex,texp),
+    id = class,
+    use_package = 'nlme',
+    quite = T
+  ),regexp = 'optim')
+  summary = expect_error(expect_warning(model_summary(model,
+                          return_result = T,
+                          assumption_plot = T,
+                          quite = T),
+                          regexp = 'assumption_plot does not support this model type'))
+  
+})
