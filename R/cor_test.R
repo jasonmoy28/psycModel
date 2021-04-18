319d83b7e018c0f7c4f76862582f3b042b6c2fa3
Jason Moy <jasonmoy28@gmail.com>
2021-04-18 13:53:14 +0800

    type: <feat> action: <`cor_test` re-write using the correlation package, so it supports more methods and robust standard errors>

--- R/cor_test.R
+++ R/cor_test.R
@@ -4,12 +4,11 @@
 #' This function uses the psych::corr.test function to generated the Pearson correlation table and their associated significance values.
 #'
 #' @param data data frame
-#' @param cols columns. tidyselect syntax or helpers.
-#' @param digit number of digits
-#' @param sig_test Default is raw. Options are 'adjusted' or 'raw'. Adjusted use holm adjustment method. See ?stats::p.adjust to learn why.
+#' @param cols correlation items. Support `dplyr::select()` syntax.
+#' @param digits number of digits to round to
 #' @param quite suppress printing output
-#' @param return_result return the data frame of the descriptive table
-#' @param ... additional argument for internal use
+#' @param return_result If it is set to `TRUE`, it will return the data frame of the correlation table
+#' @param ... additional arguments passed to correlation::correlation(). See ?correlation::correlation. Note that the return data.frame from correlation::correlation() must contains `r` and `p` (e.g., passing `baysesian = T` will not work)
 #'
 #' @return data frame of the correlation table
 #'
@@ -17,84 +16,55 @@
 #'
 #' @examples
 #' cor_test(iris, where(is.numeric))
-#' cor_test(iris, where(is.numeric), sig_test = "adjusted") # use adjusted correlation
+#' 
 cor_test <- function(data,
                      cols,
-                     sig_test = "raw",
-                     digit = 3,
+                     ...,
+                     digits = 3,
                      quite = F,
-                     return_result = F,
-                     ...) {
+                     method = 'pearson',
+                     p_adjust = 'holm',
+                     return_result = F) {
+  
+  if (!requireNamespace("correlation", quietly = TRUE)) {
+    stop("please install.packages('correlation')")
+  }
+  
   cols <- enquo(cols)
   data <- data %>% dplyr::select(!!cols)
-  data <- data_check(data)
 
-  cor_test_df <- data %>%
-    dplyr::mutate(dplyr::across(!!cols, as.numeric)) %>%
-    psych::corr.test()
-
-  cor_df_raw <- as.data.frame(cor_test_df$r) %>%
-    dplyr::mutate(dplyr::across(where(is.numeric), ~ format(round(., digit), nsmall = digit)))
-
-  cor_df <- cor_df_raw %>%
-    dplyr::mutate("rowname" = colnames(cor_df_raw)) %>%
-    dplyr::select("rowname", tidyselect::everything())
-
-
-  sig_df <- tidyr::as_tibble(cor_test_df$p) %>%
-    dplyr::mutate(dplyr::across(tidyselect::everything(), ~
-    dplyr::case_when(
-      . < 0.001 ~ "***",
-      . < 0.01 & . >= 0.001 ~ "**",
-      . < 0.05 & . >= 0.01 ~ "*",
-      T ~ ""
-    )))
-
-  # concat sig_df with cor_df
-  for (i in c(1:ncol(sig_df))) {
-    c_vec <- stringr::str_c(cor_df[[i + 1]], sig_df[[i]])
-    cor_df[[i + 1]] <- c_vec
-  }
-  if (sig_test == "raw") {
-    index <- 1
-    for (columns in colnames(cor_df[-1])) {
-      cor_df[1:index, columns] <- ""
-      index <- index + 1
-    }
-  } else if (sig_test == "adjusted") {
-    index <- 1
-    for (columns in colnames(cor_df)[-1]) {
-      cor_df[index:nrow(cor_df), columns] <- ""
-      index <- index + 1
-    }
-  }
-
-  dots <- list(...)
-  if (length(dots) > 0) {
-    if (dots$descriptive_table_use == T) {
-      return(cor_df)
-    }
-  }
-  cor_df <- cor_df %>% dplyr::rename(Var = .data$rowname)
-
+  cor = correlation::correlation(data = data,
+                                 method = method,
+                                 p_adjust = p_adjust,
+                                 ...)
+  cor_df = cor %>% 
+    as.data.frame() %>% 
+    select(Parameter1,Parameter2,r,p) %>% 
+    mutate(p = 
+             dplyr::case_when(
+               p < 0.001 ~ paste(format_round(r,digits = digits),"***"),
+               p < 0.01 & p >= 0.001 ~ paste(format_round(r,digits = digits)," **"),
+               p < 0.05 & p >= 0.01 ~ paste(format_round(r,digits = digits),"  *"),
+               T ~  paste(format_round(r,digits = digits),"   ")
+             )) %>% 
+    select(-r) %>% 
+    pivot_wider(names_from = Parameter1, values_from = p) %>% 
+    rename(Var = .data$Parameter2)
+  
+  cor_df = tibble::tibble(Var = colnames(data)) %>% 
+    full_join(cor_df,by = 'Var') %>% 
+    dplyr::mutate(dplyr::across(tidyselect::everything(), function(x) {tidyr::replace_na(data = x, replace = '')}))
+  
   if (quite == F) {
     super_print("underline|Model Summary")
-    super_print("Model Type = Pearson Correlation")
+    super_print("Model Type = Correlation")
+    super_print("Model Method = {method}")
+    super_print("Adjustment Method = {p_adjust}")
     cat("\n")
-    print_table(cor_df)
+    print_table(cor_df,digits = digits)
   }
 
   if (return_result == T) {
     return(cor_df)
-  }
-
-  # printing warning message, non-essential block
-  coreced_name <- NULL
-  coreced_name <- data %>%
-    dplyr::select(!where(is.numeric)) %>%
-    names()
-  if (length(coreced_name) != 0) {
-    warning_message <- paste(paste(coreced_name, collapse = ", "), "were coreced into numeric")
-    warning(warning_message)
   }
 }
--- R/descriptive_table.R
+++ R/descriptive_table.R
@@ -4,15 +4,14 @@
 #' This function generates a table of descriptive statistics (mainly using psych::describe; Revelle, 2021) and or a correlation table. User can export this to a csv file (optionally, using the file_path argument). Users can open the csv file with MS Excel then copy and paste the table into MS Word table.
 #'
 #' @param data data frame
-#' @param cols  column(s) need to be included in the table. Support `dplyr::select` syntax.
-#' @param cor_sig_test adjusted or raw. Default as adjusted. See psych::corr.test to learn more.
-#' @param cor_digit number of digit for correlation table
+#' @param cols  column(s) need to be included in the table. Support `dplyr::select()` syntax.
 #' @param descriptive_indicator Default is mean, sd, cor. Options are missing (missing value count), non_missing (non-missing value count), cor (correlation table), n, mean, sd, median, trimmed (trimmed mean), median, mad (median absolute deviation from the median), min, max, range, skew, kurtosis, se (standard error)
-#' @param descriptive_indicator_digit number of digit for the descriptive table
+#' @param digits number of digit for the descriptive table
 #' @param file_path file path for export. The function will implicitly pass this argument to the write.csv(file = file_path)
 #' @param quite suppress printing output
-#' @param return_result return the data frame of the descriptive table
+#' @param return_result If it is set to `TRUE`, it will return the data frame of the descriptive table
 #' @param streamline print streamlined output
+#' @param ... additional arguments passed to cor_test. See ?cor_test.
 #'
 #' @return data frame of the descriptive table
 #'
@@ -28,14 +27,19 @@
 #' )
 descriptive_table <- function(data,
                               cols,
-                              cor_sig_test = "raw",
-                              cor_digit = 3,
+                              ..., 
+                              digits = 3, 
                               descriptive_indicator = c("mean", "sd", "cor"),
-                              descriptive_indicator_digit = 3,
                               streamline = F,
                               quite = F,
                               return_result = F,
                               file_path = NULL) {
+  if (!requireNamespace("correlation", quietly = TRUE)) {
+    stop("please install.packages('correlation')")
+  }
+  if (!requireNamespace("psych", quietly = TRUE)) {
+    stop("please install.packages('psych')")
+  }
   cols <- enquo(cols)
   data <- data %>% dplyr::select(!!cols)
   data <- data_check(data)
@@ -44,22 +48,22 @@ descriptive_table <- function(data,
   compute_cor_table <- any(descriptive_indicator %in% "cor")
 
   # init return_df
-  return_df <- tibble::tibble(rowname = colnames(data))
-
+  return_df <- tibble::tibble(Var = colnames(data))
+  
   # compute the missing table
   if (any(descriptive_indicator %in% "missing")) {
     missing_df <- data %>%
       dplyr::summarize(dplyr::across(!!cols, ~ sum(is.na(.)))) %>%
-      tidyr::pivot_longer(tidyselect::everything(), names_to = "rowname", values_to = "missing_n")
-    return_df <- return_df %>% dplyr::full_join(missing_df, by = "rowname")
+      tidyr::pivot_longer(tidyselect::everything(), names_to = "Var", values_to = "missing_n")
+    return_df <- return_df %>% dplyr::full_join(missing_df, by = "Var")
   }
 
   # compute the non-missing table
   if (any(descriptive_indicator %in% "non_missing")) {
     non_missing_df <- data %>%
       dplyr::summarize(dplyr::across(!!cols, ~ sum(!is.na(.)))) %>%
-      tidyr::pivot_longer(tidyselect::everything(), names_to = "rowname", values_to = "non_missing_n")
-    return_df <- return_df %>% dplyr::full_join(non_missing_df, by = "rowname")
+      tidyr::pivot_longer(tidyselect::everything(), names_to = "Var", values_to = "non_missing_n")
+    return_df <- return_df %>% dplyr::full_join(non_missing_df, by = "Var")
   }
 
   # compute the descriptive table
@@ -70,23 +74,23 @@ descriptive_table <- function(data,
     descriptive_table <- psych::describe(x = data) %>%
       as.data.frame() %>%
       dplyr::select(!!descriptive_indicator) %>%
-      dplyr::mutate(dplyr::across(where(is.numeric), ~ format(round(., descriptive_indicator_digit), nsmall = descriptive_indicator_digit))) %>%
-      tibble::rownames_to_column(var = "rowname")
-    return_df <- return_df %>% dplyr::full_join(descriptive_table, by = "rowname")
+      dplyr::mutate(dplyr::across(where(is.numeric), ~ format(round(., digits), nsmall = digits))) %>%
+      tibble::rownames_to_column(var = "Var")
+    return_df <- return_df %>% dplyr::full_join(descriptive_table, by = "Var")
   }
   # compute the correlation table
   if (compute_cor_table == T) {
-    cor_table <- data %>% cor_test(cols = !!cols, sig_test = cor_sig_test, digit = cor_digit, descriptive_table_use = T)
-    return_df <- return_df %>% dplyr::full_join(cor_table, by = "rowname")
+    cor_table <- data %>% cor_test(cols = !!cols, return_result = T,quite = T,...)
+    return_df <- return_df %>% dplyr::full_join(cor_table, by = "Var")
   }
-
-  return_df <- return_df %>% dplyr::rename(Var = .data$rowname)
+  
   if (quite == F) {
     if (streamline == F) {
       cat("\n")
       super_print("underline|Model Summary")
       super_print("Model Type = Descriptive Statistics")
     }
+    cat("\n")
     print_table(return_df)
     cat("\n")
   }
