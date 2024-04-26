eb04b7b388681931de405ebd315e9f32c01574ac
Jason Moy <jasonmoy28@gmail.com>
2024-04-26 15:54:20 -0700

    type: <fix> message: <deleted lme_model and lme_model_summary>

--- tests/testthat/test-lme-model.R
+++
@@ -1,8 +0,0 @@
-testthat::test_that(desc = "lme_model: lmerTest (specify model)", {
-  fit <- lme_model(
-    data = popular,
-    model = "popular ~ extrav + sex + (1 | class)",
-    use_package = "lmerTest"
-  )
-  expect_equal(class(fit)[1], "lmerModLmerTest")
-})
